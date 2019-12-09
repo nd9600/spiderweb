a668db865fc9e1f79182e475e478865084f0a435
Nathan Douglas <nd9600@gmail.com>
2019-12-09 21:37:57 +0000

    added favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,8 +2,24 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport"
-        content="width=device-width, initial-scale=1">
+    <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1"
+    >
+
+    <link rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png">
+    <link rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png">
+    <link rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png">
+    <link rel="manifest"
+        href="/site.webmanifest">
 
     <!-- CSRF Token -->
     <meta name="csrf-token"
@@ -16,10 +32,8 @@
     <!-- Scripts -->
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}"
-        rel="stylesheet">
-    <link href="{{ asset('css/tailwind.min.css') }}"
-        rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/tailwind.min.css') }}" rel="stylesheet">
 </head>
 <body>
 <div id="app">
