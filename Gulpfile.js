3264d69429d042011ae530ad5d0fdba3002c3fa0
Nathan Douglas <nd9600@gmail.com>
2019-12-14 12:46:07 +0000

    got styles revisioning working

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,30 +1,61 @@
-// const fs = require("fs");
-// const babel = require("gulp-babel");
-// const concat = require("gulp-concat");
-// const declare = require("gulp-declare");
 const gulp = require("gulp");
-// const gutil = require("gulp-util");
+const gutil = require("gulp-util");
 // const named = require("vinyl-named-with-path");
-// const path = require("path");
-// const rev = require("gulp-rev");
-// const revOutdated = require("gulp-rev-outdated");
-// const rimraf = require("rimraf");
+const path = require("path");
+const through = require("through2");
+const rev = require("gulp-rev");
+const revOutdated = require("gulp-rev-outdated");
+const rimraf = require("rimraf");
 const sass = require("gulp-sass");
 const cssimport = require("gulp-cssimport");
-// const through = require("through2");
-// const uglify = require("gulp-uglify");
 // const webpackstream = require("webpack-stream");
-// const wrap = require("gulp-wrap");
 
 sass.compiler = require('node-sass');
 
-function defaultTask(cb) {
-    // place code for your default task here
-    cb();
+/**
+ * from https://www.npmjs.com/package/gulp-rev-outdated
+ */
+function cleaner() {
+    return through.obj(function (file, enc, cb) {
+        rimraf(path.resolve((file.cwd || process.cwd()), file.path), function (err) {
+            if (err) {
+                this.emit('error', new gutil.PluginError('Cleanup old files', err));
+            }
+            this.push(file);
+            cb();
+        }.bind(this));
+    });
+}
+
+/**
+ * Takes an input stream, revisions all files in the stream, writes them, builds a manifest file, merges the manifest
+ * file with any existing manifest, writes the manifest file.
+ *
+ * @param inputStream
+ * @returns {*}
+ */
+function revision(inputStream) {
+    return inputStream.pipe(rev())
+        .pipe(gulp.dest("./public")) // output the public files into public/
+        .pipe(rev.manifest({
+            merge: true
+        }))
+        .pipe(gulp.dest(".")); // output the manifest into ./
+}
+
+/**
+ * Removes outdated file versions
+ */
+function clean(cb) {
+    return gulp.src("./public/assets/**/*", {read: false})
+        .pipe(revOutdated(1))
+        .pipe(cleaner());
 }
 
 function styles(cb) {
-    gulp
+    // bundle and minify the design system
+    revision(
+        gulp
         .src(
             [
                 "./resources/assets/css/app.css",
@@ -38,23 +69,22 @@ function styles(cb) {
             sass({outputStyle: "compressed"})
                 .on("error", sass.logError)
         )
-        .pipe(
-            gulp.dest("./public/")
-        );
+    );
 
-    return gulp.src(
-        [
-            "./resources/assets/css/tailwind.min.css",
-        ],
-        {
-            base: "./resources" // we want to put the files in "./public/assets/css", so we only want to remove the "resources"
-        }
-    )
-        .pipe(
-            gulp.dest("./public/")
-        );
+    // copy over tailwind
+    return revision(
+        gulp.src(
+            [
+                "./resources/assets/css/tailwind.min.css",
+            ],
+            {
+                base: "./resources" // we want to put the files in "./public/assets/css", so we only want to remove the "resources"
+            }
+        )
+    );
 }
 
 
+exports.clean = clean;
 exports.styles = styles;
-exports.default = defaultTask;
+exports.default = gulp.series(styles, clean);
