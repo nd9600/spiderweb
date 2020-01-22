18d9f837e29e93c73db1b2f7e165993d03108281
Nathan Douglas <nd9600@gmail.com>
2020-01-22 14:11:41 +0000

    fixed whitespace

--- resources/assets/js/commonComponents/Post.vue
+++ resources/assets/js/commonComponents/Post.vue
@@ -2,9 +2,9 @@
     <section>
         <h1
             v-if="post.title.length > 0"
-            class="h h--1 whitespace-pre"
+            class="h h--1 whitespace-pre-wrap"
         >{{ post.title }}</h1>
-        <p class="whitespace-pre">{{ post.body }}</p>
+        <p class="whitespace-pre-wrap">{{ post.body }}</p>
     </section>
 </template>
 
@@ -15,15 +15,6 @@ export default {
         post: {
             type: Object,
             required: true
-            /*
-            {
-                    "user_id": 1,
-                    "title": "Foo",
-                    "body": "Foo body",
-                    "created_at": moment().format(),
-                    "updated_at": moment().format(),
-                }
-             */
         }
     }
 };
