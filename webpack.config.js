47f0ad17194de29ffe6d907981fc07826a459cfd
Nathan Douglas <nd9600@gmail.com>
2020-01-26 21:11:21 +0000

    got webpack's prod mode working

--- webpack.config.js
+++ webpack.config.js
@@ -3,112 +3,116 @@ const webpack = require("webpack");
 const VueLoaderPlugin = require("vue-loader/lib/plugin");
 const WebpackAssetsManifest = require("webpack-assets-manifest");
 
-const mode = process.env.mode;
-
 const entrypoints = {
     "js/offline/graph": "./resources/assets/js/offline/graph.js"
 };
 
-module.exports = {
-    mode: mode,
+module.exports = (env, argv) => {
+    const mode = argv.mode;
 
-    entry: entrypoints,
+    let config = {
+        mode: mode,
 
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: [
-                    "vue-style-loader",
-                    "css-loader"
-                ],
-            }, {
-                test: /\.vue$/,
-                loader: "vue-loader",
-                options: {
-                    loaders: {}
-                }
-            },
-            {
-                test: /\.js$/,
-                loader: "babel-loader",
-                exclude: /node_modules/,
-                options: {
-                    presets: ["@babel/preset-env"],
-                }
-            },
-            {
-                test: /\.(png|jpg|gif|svg)$/,
-                loader: "file-loader",
-                options: {
-                    name: "[name].[ext]"
-                }
-            }
-        ]
-    },
+        entry: entrypoints,
 
-    resolve: {
-        alias: {
-            "vue$": "vue/dist/vue.esm.js",
-            "@": path.resolve(__dirname, "resources/assets"),
+        module: {
+            rules: [
+                {
+                    test: /\.css$/,
+                    use: [
+                        "vue-style-loader",
+                        "css-loader"
+                    ],
+                }, {
+                    test: /\.vue$/,
+                    loader: "vue-loader",
+                    options: {
+                        loaders: {}
+                    }
+                },
+                {
+                    test: /\.js$/,
+                    loader: "babel-loader",
+                    exclude: /node_modules/,
+                    options: {
+                        presets: ["@babel/preset-env"],
+                    }
+                },
+                {
+                    test: /\.(png|jpg|gif|svg)$/,
+                    loader: "file-loader",
+                    options: {
+                        name: "[name].[ext]"
+                    }
+                }
+            ]
         },
-        extensions: ["*", ".js", ".vue", ".json"]
-    },
-    performance: {
-        hints: false
-    },
-    optimization: {
-        splitChunks: {
-            cacheGroups: {
-                vendor: {
-                    test: /[\\/]node_modules[\\/]/,
-                    name: "js/vendors",
-                    chunks: "all",
-                    minChunks: 1
+
+        resolve: {
+            alias: {
+                "vue$": "vue/dist/vue.esm.js",
+                "@": path.resolve(__dirname, "resources/assets"),
+            },
+            extensions: ["*", ".js", ".vue", ".json"]
+        },
+        performance: {
+            hints: false
+        },
+        optimization: {
+            splitChunks: {
+                cacheGroups: {
+                    vendor: {
+                        test: /[\\/]node_modules[\\/]/,
+                        name: "js/vendors",
+                        chunks: "all",
+                        minChunks: 1
+                    }
                 }
             }
-        }
-    },
+        },
 
-    stats: {
-        colors: true
-    },
+        stats: {
+            colors: true
+        },
 
-    plugins: [
-        new VueLoaderPlugin(),
-        new webpack.HashedModuleIdsPlugin(),
-        new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/), // we don't need any of moment's locales,,
+        plugins: [
+            new VueLoaderPlugin(),
+            new webpack.HashedModuleIdsPlugin(),
+            new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/), // we don't need any of moment's locales
 
-        new WebpackAssetsManifest({
-            output: path.resolve(__dirname, "rev-manifest.json"),
-            merge: true
-        })
-    ],
+            new WebpackAssetsManifest({
+                output: path.resolve(__dirname, "rev-manifest.json"),
+                merge: true
+            })
+        ],
 
-    // devtool: false,
-    devtool: mode === "production"
-        ? "source-map" // "false"
-        : "eval-source-map",
+        // devtool: false,
+        devtool: mode === "production"
+            ? "source-map"
+            : "eval-source-map",
 
-    output: {
-        filename: mode === "production"
-            ? "[name]-[hash].js" // "false"
-            : "[name].js",
-        path: path.resolve(__dirname, "public/assets/"),
-        publicPath: "/",
-    },
+        output: {
+            filename: mode === "production"
+                ? "[name]-[hash].js" // "false"
+                : "[name].js",
+            path: path.resolve(__dirname, "public/assets/"),
+            publicPath: "/",
+        },
+    };
+
+    if (mode === "production") {
+        // http://vue-loader.vuejs.org/en/workflow/production.html
+        config.plugins = (config.plugins || []).concat([
+            new webpack.DefinePlugin({
+                "process.env": {
+                    NODE_ENV: '"production"'
+                }
+            }),
+            new webpack.LoaderOptionsPlugin({
+                minimize: true
+            })
+        ]);
+    }
+
+    return config;
 };
-
-if (mode === "production") {
-    // http://vue-loader.vuejs.org/en/workflow/production.html
-    module.exports.plugins = (module.exports.plugins || []).concat([
-        new webpack.DefinePlugin({
-            "process.env": {
-                NODE_ENV: '"production"'
-            }
-        }),
-        new webpack.LoaderOptionsPlugin({
-            minimize: true
-        })
-    ]);
-}
