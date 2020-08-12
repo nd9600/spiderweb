1e91fcfcf073c78153ea4d1568ede4ff0911846d
Nathan Douglas <nd9600@gmail.com>
2020-08-12 09:00:12 +0100

    started buttons to jump you to a post in the bar

--- resources/assets/js/commonComponents/Viewer/PostBar/PostBar.vue
+++ resources/assets/js/commonComponents/Viewer/PostBar/PostBar.vue
@@ -1,30 +1,55 @@
 <template>
     <section>
-        <div
-            id="postBar"
-            class="w-full flex items-start overflow-x-auto postBar"
-        >
-            <post
-                v-for="selectedPostId in selectedPostIds"
-                :key="selectedPostId"
-                class="m-2 p-2"
-                :post="posts[selectedPostId]"
+        <div class="postBar">
+            <div class="flex justify-between items-center">
+                <button
+                    v-if="!visiblePosts.includes(0)"
+                    class="btn btn--secondary"
+                    type="button"
+                >
+                    <span class="text-2xl">⇐</span>
+                </button>
+                <span>{{visiblePosts}}</span>
+                <button
+                    v-if="!visiblePosts.includes(selectedPostIds.length - 1)"
+                    class="btn btn--secondary"
+                    type="button"
+                >
+                    <span class="text-2xl">⇒</span>
+                </button>
+            </div>
+            <div
+                id="postBar"
+                class="w-full flex items-start overflow-x-auto"
             >
-            </post>
+                <post
+                    v-for="(selectedPostId, i) in selectedPostIds"
+                    :id="`post-${i}`"
+                    :key="selectedPostId"
+                    class="m-2 p-2"
+                    :post="posts[selectedPostId]"
+                >
+                </post>
+            </div>
         </div>
     </section>
 </template>
 
 <script>
-import Post from "./Post";
-
 import { mapState, mapMutations } from "vuex";
+import elementIsVisible from "@/js/helpers/elementIsVisible.js";
 
+import Post from "./Post";
 
 export default {
     name: "PostBar",
     components: {
         Post,
+    },
+    data() {
+        return {
+            visiblePosts: []
+        };
     },
     computed: {
         ...mapState("dataModule", ["posts"]),
@@ -37,8 +62,26 @@ export default {
             }
         }
     },
+    mounted() {
+        this.setVisiblePosts();
+        document.getElementById("postBar").onscroll = this.setVisiblePosts;
+    },
     methods: {
-        ...mapMutations("dataModule", ["setSelectedPostIds"])
+        ...mapMutations("dataModule", ["setSelectedPostIds"]),
+
+        setVisiblePosts() {
+            let visiblePosts = [];
+            for (let i = 0; i < this.selectedPostIds.length; i++) {
+                const postIsVisible = elementIsVisible(
+                    document.getElementById(`post-${i}`),
+                    document.getElementById("postBar")
+                );
+                if (postIsVisible) {
+                    visiblePosts.push(i);
+                }
+            }
+            this.visiblePosts = visiblePosts;
+        }
     },
 };
 </script>
