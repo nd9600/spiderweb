9150ad14be5ff92b4f7dac46eef13caf63c358af
MentatBot <160964065+MentatBot@users.noreply.github.com>
2025-08-08 12:36:14 +0000

    fix(vite): add .vue extension to OfflineRoot import for Vite resolver
    Co-authored-by: nd9600 <9141675+nd9600@users.noreply.github.com>

--- resources/assets/js/src/offline/graph.js
+++ resources/assets/js/src/offline/graph.js
@@ -2,7 +2,7 @@ import Vue from "vue";
 import "../../../css/app.css";
 import "../../../css/tailwind.min.css";
 
-import OfflineRoot from "./OfflineRoot";
+import OfflineRoot from "./OfflineRoot.vue";
 import store from "./store";
 
 new Vue({
