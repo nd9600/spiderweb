9557ee3e517ef996984386f0aa2406c33125ff56
Nathan Douglas <nd9600@gmail.com>
2025-08-09 23:18:15 +0100

    WIP migration

--- spiderweb/src/App.vue
+++ spiderweb/src/App.vue
@@ -1,17 +1,31 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+    <div>
+        <a
+            href="https://vite.dev"
+            target="_blank"
+        >
+            <img
+                src="/vite.svg"
+                class="logo"
+                alt="Vite logo"
+            />
+        </a>
+        <a
+            href="https://vuejs.org/"
+            target="_blank"
+        >
+            <img
+                src="./assets/vue.svg"
+                class="logo vue"
+                alt="Vue logo"
+            />
+        </a>
+    </div>
+    <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
--- spiderweb/src/components/HelloWorld.vue
+++ spiderweb/src/components/HelloWorld.vue
@@ -11,7 +11,7 @@ const count = ref(0)
 
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
+    <p class="text-red-500">
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
     </p>
