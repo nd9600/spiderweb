fdcc4bd312016478c5dceabd565fc4a2da510994
Nathan Douglas <nd9600@gmail.com>
2025-08-09 23:32:18 +0100

    WIP migration

--- spiderweb/src/components/HelloWorld.vue
+++ spiderweb/src/components/HelloWorld.vue
@@ -11,7 +11,7 @@ const count = ref(0)
 
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
-    <p class="text-red-500">
+    <p class="text-red">
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
     </p>
--- spiderweb/vite.config.ts
+++ spiderweb/vite.config.ts
@@ -4,8 +4,13 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-      vue(),
-      tailwindcss(),
-  ],
+    plugins: [
+        vue(),
+        tailwindcss(),
+    ],
+    resolve: {
+        alias: {
+            '@': resolve(__dirname, 'src/js/')
+        }
+    },
 })
