c8c13de9bc09f848595397316bf93f3136d4dc10
Nathan Douglas <nd9600@gmail.com>
2026-07-22 17:53:50 +0100

    removed test

--- resources/assets/js/__tests__/store/recordIds.test.ts
+++
@@ -1,5 +0,0 @@
-import {newRecordId} from "@/src/store/modules/dataModule/shared";
-
-test("new record IDs use the next integer string", () => {
-    expect(newRecordId(["1", "7", "not-an-integer"])).toBe("8");
-});
